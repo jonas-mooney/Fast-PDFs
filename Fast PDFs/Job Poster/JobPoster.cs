dab7ed39437f39ee9e32613f2b86a3a60f4e3f4b
Jonas <jonasmooney2@gmail.com>
2025-06-02 08:11:06 -0600

    JobPoster class improved

--- Fast PDFs/Job Poster/JobPoster.cs
+++ Fast PDFs/Job Poster/JobPoster.cs
@@ -2,14 +2,10 @@
 {
     public class JobPoster
     {
-        // name of company, job title, option to include salary or hourly wage, full time or part time w/ hours, description, qualifications, job requirements, contact info
-
         public string CompanyName { get; set; }
         public string JobTitle { get; set; }
         public string JobDescription { get; set; }
-        public string SalaryOrHourly { get; set; }
-        public int Salary { get; set; }
-        public int HourlyWage { get; set; }
+        public Compensation Compensation { get; set; }
         public string FullOrPartTime { get; set; }
         public string[] Qualifications { get; set; }
         public string[] JobRequirements { get; set; }
@@ -17,9 +13,30 @@
         public string ContactEmail { get; set; }
         public string Url { get; set; }
 
-        public JobPoster(string companyName, string jobTitle, string jobDescription, string salaryOrHourly, int salary, int hourlyWage)
+        public JobPoster(string companyName, string jobTitle, string jobDescription, Compensation compensation, string fullOrPartTime, string[] qualifications, string[] jobRequirements, string contactPhone, string contactEmail, string url)
         {
             CompanyName = companyName;
+            JobTitle = jobTitle;
+            JobDescription = jobDescription;
+            Compensation = compensation;
+            FullOrPartTime = fullOrPartTime;
+            Qualifications = qualifications;
+            JobRequirements = jobRequirements;
+            ContactPhone = contactPhone;
+            ContactEmail = contactEmail;
+            Url = url;
+        }
+    }
+
+    public class Compensation
+    {
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+
+        public Compensation(string type, decimal amount)
+        {
+            Type = type;
+            Amount = amount;
         }
     }
 }
